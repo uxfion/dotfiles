ceb56e9d72bbdc80e4822852810d85c2c7df4a07
Lecter <uxfion@gmail.com>
2022-11-27 18:38:39 +0800

    自动更新插件无法使用

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -15,17 +15,6 @@ autocmd("BufEnter", {
     end,
 })
 
--- 自动切换输入法，需要安装 im-select
--- https://github.com/daipeihust/im-select
---autocmd("InsertLeave", {
---    group = myAutoGroup,
---    callback = require("utils.im-select").macInsertLeave,
---})
---autocmd("InsertEnter", {
---    group = myAutoGroup,
---    callback = require("utils.im-select").macInsertEnter,
---})
-
 -- 进入Terminal 自动进入插入模式
 autocmd("TermOpen", {
     group = myAutoGroup,
--- .config/nvim/lua/plugin-config/null-ls.lua
+++ .config/nvim/lua/plugin-config/null-ls.lua
@@ -17,18 +17,18 @@ null_ls.setup({
         formatting.prettier,
     },
     -- 保存自动格式化
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    -- vim.lsp.buf.formatting_sync()
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                end,
-            })
-        end
-    end,
+    --    on_attach = function(client, bufnr)
+    --        if client.supports_method("textDocument/formatting") then
+    --            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    --            vim.api.nvim_create_autocmd("BufWritePre", {
+    --                group = augroup,
+    --                buffer = bufnr,
+    --                callback = function()
+    --                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+    --                    -- vim.lsp.buf.formatting_sync()
+    --                    vim.lsp.buf.format({ bufnr = bufnr })
+    --                end,
+    --            })
+    --        end
+    --    end,
 })
